dbcaf430d80348551a02c4f49e023e399dfc8c01
Marcin Malinowski <mrma@jcommerce.pl>
2018-11-22 13:33:51 +0100

    pierwszy program hello tablica args

--- hello/Program.cs
+++ hello/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace hello
 {
@@ -10,6 +7,15 @@ namespace hello
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+            Console.WriteLine("asd" + " " + args[0] + " " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("wwitaj " + item);
+            //}
+
+
         }
     }
 }
